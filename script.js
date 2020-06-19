b6d77002e489d4c73b5e783ca5a46e509411bbdf
Rk7370 <65388072+Rk7370@users.noreply.github.com>
2020-06-19 19:00:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-    <script>
-        var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
         for(var i=0; i<names.length; i++)
         {
             if(names[i][0]=="J" || names[i][0]=="j"){
@@ -9,4 +8,3 @@
                 console.log("Hello "+names[i]);
             }
         }
-    </script>
