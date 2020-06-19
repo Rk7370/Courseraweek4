9568b4ddd2fafb4a894d730ab1883b5bb9f4c6af
Rk7370 <65388072+Rk7370@users.noreply.github.com>
2020-06-19 18:57:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+    <script>
+        var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+        for(var i=0; i<names.length; i++)
+        {
+            if(names[i][0]=="J" || names[i][0]=="j"){
+                console.log("GoodBye "+ names[i]);
+            }
+            else{
+                console.log("Hello "+names[i]);
+            }
+        }
+    </script>
